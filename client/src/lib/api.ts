174b38ab2af72438a36052c6172a40ac17963b9a
ToxicWixorg <m.salehifar980@gmail.com>
2026-05-07 11:37:30 +0330

    update

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -11,7 +11,7 @@ import axios from "axios";
 const DEV_BYPASS = import.meta.env.VITE_DEV_BYPASS === "true";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? "/api",
+  baseURL: import.meta.env.VITE_API_URL ?? "",
   headers: { "Content-Type": "application/json" },
 });
 
